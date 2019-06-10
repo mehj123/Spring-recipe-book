bd18524ec25b54d76956afe0f906fe7a99907e9e
mehj123 <mehjabeen.begum@outlook.com>
2019-06-10 16:43:09 +0400

    Adding POJO classes

--- src/main/java/com/learning/recipebook/controllers/IndexController.java
+++ src/main/java/com/learning/recipebook/controllers/IndexController.java
@@ -1,4 +1,14 @@
 package com.learning.recipebook.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("helo 123");
+        return "index";
+    }
 }
