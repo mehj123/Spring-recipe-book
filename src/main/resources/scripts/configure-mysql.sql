164e231a5bd8b5f989bc6746e0af94a64cd69335
mehj123 <mehjabeen.begum@outlook.com>
2019-08-04 12:18:39 +0400

    Setting up schema generation with Hibernate

---
+++ src/main/resources/scripts/configure-mysql.sql
@@ -0,0 +1,18 @@
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'password';
+
+
+#Database grants
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
